4421cdb22aae8299479c99bec32982323921e635
pawarakshay136 <pawarskshay136@gmail.com>
2024-11-28 16:13:57 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,59 +1,165 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.logging.LogType;
-import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import demo.wrappers.Wrappers;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.time.Duration;
+import java.io.File;
 
 public class TestCases {
+
     ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
+     // Method to start the browser before tests
+     @BeforeTest
+     public void startBrowser() {
+         System.setProperty("java.util.logging.config.file", "logging.properties");
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("--remote-allow-origins=*");
+ 
+         driver = new ChromeDriver(options);
+         driver.manage().window().maximize();
+     }
 
-     
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
-    @BeforeTest
-    public void startBrowser()
-    {
-        System.setProperty("java.util.logging.config.file", "logging.properties");
+    // Test case to scrape hockey teams data and store in JSON
+    @Test
+    public void testCase01() throws InterruptedException, IOException {
+        ObjectMapper mapper = new ObjectMapper(); // Jackson ObjectMapper for JSON conversion
+        driver.get("https://www.scrapethissite.com/pages/");
+        HashMap<Integer, Object> map = new HashMap<>();
+        
+        // Clicking on the "Hockey Teams" page
+        Wrappers.clickHockeyTeams(driver);
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+        int k = 1, n = 0;
 
-        ChromeOptions options = new ChromeOptions();
-        LoggingPreferences logs = new LoggingPreferences();
+        // Loop to iterate through multiple pages (4 pages as per the requirement)
+        while (n < 4) {
+            int rows = driver.findElements(By.xpath("//table/tbody/tr")).size();
+            
+            // Iterating through the rows of the table
+            for (int i = 2; i < rows; i++) {
+                String winPercentage = driver.findElement(By.xpath("//table/tbody/tr[" + i + "]/td[6]")).getText();
+                float winPercent = Float.parseFloat(winPercentage);
+                
+                // Only consider teams with win percentage less than 40%
+                if (winPercent < 0.4) {
+                    ArrayList<String> list = new ArrayList<>();
+                    String team = driver.findElement(By.xpath("//table/tbody/tr[" + i + "]/td[1]")).getText();
+                    String year = driver.findElement(By.xpath("//table/tbody/tr[" + i + "]/td[2]")).getText();
 
-        logs.enable(LogType.BROWSER, Level.ALL);
-        logs.enable(LogType.DRIVER, Level.ALL);
-        options.setCapability("goog:loggingPrefs", logs);
-        options.addArguments("--remote-allow-origins=*");
+                    // Adding epoch time, team name, year, and win percentage to the list
+                    list.add(Long.toString(Wrappers.getEpochTime()));
+                    list.add(team);
+                    list.add(year);
+                    list.add(winPercentage);
+                    
+                    // Storing in the map with key as an incremented integer
+                    map.put(k, list);
+                    k++;
+                }
+            }
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+            // Click "Next" to move to the next page
+            Wrappers.clickNext(driver);
+            n++;
+        }
 
-        driver = new ChromeDriver(options);
+        // Print out the collected data to console
+        for (Integer i : map.keySet()) {
+            System.out.println(map.get(i));
+        }
 
-        driver.manage().window().maximize();
+        // Create the JSON file
+        Wrappers.createJsonFile(mapper, map);
+        String userDir = System.getProperty("user.dir");
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(userDir + "\\src\\test\\resources\\hockey-team-data.json"), map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    // Test case to scrape Oscar-winning films data and store in JSON
+    @Test
+    public void testCase02() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        int q = 1;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://www.scrapethissite.com/pages/");
+        HashMap<Integer, Object> map = new HashMap<>();
+
+        // Clicking on the "Oscar Winning Films" page
+        Wrappers.clickOscarWinning(driver);
+
+        // Iterating through the years of the Oscar winners
+        for (WebElement yearElement : Wrappers.getYears(driver)) {
+            String year = yearElement.getText();
+            yearElement.click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//th[text()='Title']")));
+            
+            // Loop through the top 5 movies for the current year
+            for (int j = 1; j <= 5; j++) {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(Long.toString(Wrappers.getEpochTime()));
+                list.add(year);
+
+                // Collecting details for each movie
+                for (int k = 1; k <= 4; k++) {
+                    if (k < 4) {
+                        String movieDetail = driver.findElement(By.xpath("//table/tbody/tr[" + j + "]/td[" + k + "]")).getText();
+                        list.add(movieDetail);
+                    } else {
+                        try {
+                            // Check if the movie is the Best Picture winner
+                            Boolean isWinner = driver.findElement(By.xpath("//table/tbody/tr[" + j + "]/td[" + k + "]/i")).isDisplayed();
+                            list.add(String.valueOf(isWinner));
+                        } catch (Exception e) {
+                            list.add("false");
+                        }
+                    }
+                }
+
+                // Add the movie data to the map
+                map.put(q, list);
+                q++;
+            }
+        }
+
+        // Print out the collected Oscar winner data
+        for (Integer i : map.keySet()) {
+            System.out.println(map.get(i));
+        }
+
+        // Create the JSON file
+        Wrappers.createJsonFile(mapper, map);
+        String userDir = System.getProperty("user.dir");
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(userDir + "\\src\\test\\resources\\oscar-winner-data.json"), map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+   
+    // Method to close the browser after tests
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
-
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,63 @@
 package demo.wrappers;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.openqa.selenium.Alert;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+    // Method to get the current epoch time (in seconds)
+    public static long getEpochTime() {
+        long epochSeconds = Instant.now().getEpochSecond();
+        return epochSeconds;
+    }
+
+    // Method to click the "Next" button for pagination
+    public static void clickNext(WebDriver driver) throws InterruptedException {
+        WebElement ele = driver.findElement(By.xpath("//a[@aria-label='Next']"));
+        ele.click();
+        Thread.sleep(3000); // wait for the next page to load
+    }
+
+    // Method to click on the "Hockey Teams: Forms, Searching and Pagination" link
+    public static void clickHockeyTeams(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement link = driver.findElement(By.xpath("//a[text()='Hockey Teams: Forms, Searching and Pagination']"));
+        link.click();
+        wait.until(ExpectedConditions.urlContains("/pages/forms/"));
+    }
+
+    // Method to click on the "Oscar Winning Films" link
+    public static void clickOscarWinning(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement link = driver.findElement(By.xpath("//a[text()='Oscar Winning Films: AJAX and Javascript']"));
+        link.click();
+        wait.until(ExpectedConditions.urlContains("ajax-javascript"));
+    }
+
+    // Method to get a list of all year elements on the Oscar Winning Films page
+    public static List<WebElement> getYears(WebDriver driver) {
+        return driver.findElements(By.xpath("//a[@class='year-link']"));
+    }
+
+    // Method to create a JSON file from a HashMap object using Jackson
+    public static void createJsonFile(ObjectMapper mapper, HashMap<Integer, Object> map) throws IOException {
+        try {
+            String Json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+            System.out.println(Json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
